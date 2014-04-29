df9f12d21776a739177802efbcb7cb4159b40d37
Austin Zheng <austinzheng@gmail.com>
2014-04-28 21:05:23 -0700

    Initial Commit

---
+++ Vaporeon/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  Vaporeon
+//
+//  Created by Austin Zheng on 4/28/14.
+//  Copyright (c) 2014 Austin Zheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        while (YES) {
+            // Don't do anything
+        }
+    }
+    return 0;
+}
+
